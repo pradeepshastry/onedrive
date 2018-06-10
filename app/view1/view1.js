ea6042a51f737a3732eeb0137ec5a9acdf22e452
Pradeep Prakash <pradeep.prakash@pearson.com>
2018-06-10 13:22:42 -0700

    initial commit

---
+++ app/view1/view1.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+.controller('View1Ctrl', [function() {
+
+}]);
---
+++ app/view2/view2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+}])
+
+.controller('View2Ctrl', [function() {
+
+}]);
