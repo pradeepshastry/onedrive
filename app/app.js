5deb2ce3f77f75b9848fc6a2904dfdcc867d70f8
Pradeep Prakash <pradeep.prakash@pearson.com>
2018-06-10 18:23:34 -0700

    sasa

--- app/app.js
+++ app/app.js
@@ -17,5 +17,7 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
       templateUrl: 'view2/view2.html',
       controller: 'View2Ctrl'
   })
+  .when('/view3', {
+  })
   .otherwise({redirectTo: '/view1'});
 }]);
