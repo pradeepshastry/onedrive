50c3f6ba4187c467739bc192119cdda791681cba
Pradeep Prakash <pradeep.prakash@pearson.com>
2018-06-10 17:00:44 -0700

    changed

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute'])
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.launchOneDrivePicker = function(){
+        var odOptions = {
+            clientId: "54c44c3c-7426-4e7e-85f9-113107cd22a0",
+            action: "download",
+            advanced: {
+                redirectUri: "https://pradeepshastry.github.io/onedrive/app/index.html"
+            },
+            success: function(files) { /* success handler */ },
+            cancel: function() { /* cancel handler */ },
+            error: function(e) { /* error handler */ }
+        }
+        OneDrive.open(odOptions);
+    }
 }]);
