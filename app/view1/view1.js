25aea8472afa653ab4462361a377a56ce0c69c86
Pradeep Prakash <pradeep.prakash@pearson.com>
2018-07-17 23:12:51 -0700

    settign values

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -4,15 +4,19 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', ['$scope', function($scope) {
     $scope.launchOneDrivePicker = function(){
         var odOptions = {
+            // clientId: "df5a2538-00b8-4663-b1b5-62df4b7fa9d0",
             clientId: "54c44c3c-7426-4e7e-85f9-113107cd22a0",
             action: "share",
             advanced: {
                 redirectUri: "https://pradeepshastry.github.io/onedrive/app/index.html"
-                // redirectUri: "http://localhost:8000/app/index.html"
+                // redirectUri: "http://localhost:4000/app",
+                // createLinkParameters: { type: "edit", share: "anonymous" },
             },
             success: function(files) {
                 console.log('Success Pradeep');
+                $scope.response = files;
                 console.log(files)
+                $scope.$apply();
             },
             cancel: function() { /* cancel handler */ },
             error: function(e) { /* error handler */ }
