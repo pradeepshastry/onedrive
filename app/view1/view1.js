5af3cd94fd793e5a4746dd72c2eedfde63c737c1
Pradeep Prakash <pradeep.prakash@pearson.com>
2018-07-23 16:45:46 -0700

    added comma

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -8,7 +8,7 @@ angular.module('myApp.view1', ['ngRoute'])
             clientId: "54c44c3c-7426-4e7e-85f9-113107cd22a0",
             action: "share",
             advanced: {
-                redirectUri: "https://pradeepshastry.github.io/onedrive/app/index.html"
+                redirectUri: "https://pradeepshastry.github.io/onedrive/app/index.html",
                 // redirectUri: "http://localhost:4000/app",
                 createLinkParameters: { type: "edit", share: "organization" },
             },
