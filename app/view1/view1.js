afe6b8533e7a3b99d76d1cba7b5b1d05792e199c
Pradeep Prakash <pradeep.prakash@pearson.com>
2018-08-02 14:46:57 -0700

    changed share type to org

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,7 +10,7 @@ angular.module('myApp.view1', ['ngRoute'])
             advanced: {
                 redirectUri: "https://pradeepshastry.github.io/onedrive/app/index.html",
                 // redirectUri: "http://localhost:4000/app",
-                createLinkParameters: { type: "edit", share: "anonymous" },
+                createLinkParameters: { type: "edit", share: "organizational" },
             },
             success: function(files) {
                 console.log('Success Pradeep');
